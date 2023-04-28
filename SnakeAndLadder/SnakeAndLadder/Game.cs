dd799ef861912ff0f16afe08c91a8dc29e356754
“Nikhlesh117” <nikhlesh.rao@gmail.com>
2023-04-28 10:48:19 +0530

    Added: Single Player At Start Position

---
+++ SnakeAndLadder/SnakeAndLadder/Game.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder
+{
+    public class Game
+    {
+        public void SnakeAndLadderGame()
+        {
+            int position = 0;
+
+            Console.WriteLine("\nWelcome Player1");
+            Console.WriteLine("Player1 at position : " + position);
+        }
+    }
+}
---
+++ SnakeAndLadder/SnakeAndLadder/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace SnakeAndLadder
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Snake and Ladder Game");
+
+            Game game = new Game();
+            game.SnakeAndLadderGame();
+        }
+    }
+}
