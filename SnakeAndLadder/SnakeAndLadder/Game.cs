5603db72be80cdfc4675dbdb1730a6a6ded547bb
“Nikhlesh117” <nikhlesh.rao@gmail.com>
2023-04-28 11:42:09 +0530

    Added: Repeat till the Player reaches the winning position 100.

--- SnakeAndLadder/SnakeAndLadder/Game.cs
+++ SnakeAndLadder/SnakeAndLadder/Game.cs
@@ -11,29 +11,36 @@ namespace SnakeAndLadder
         public void SnakeAndLadderGame()
         {
             int position = 0;
+            int temp = 0;
             Console.WriteLine("\nWelcome Player");
-            Console.WriteLine("\nPlayer at position : " + position);
-            int dice;
+            Console.WriteLine("Player at position : " + position);
             Random random = new Random();
-            dice = random.Next(1, 7);
-            Console.WriteLine("\nPlayer rolls dice :" + dice);
-            int option;
-            option = random.Next(0, 3);
-            switch (option)
+            while (position < 100)
             {
-                case 0:
-                    Console.WriteLine("No Play");
-                    break;
-                case 1:
-                    Console.WriteLine("Ladder");
-                    position += dice;
-                    break;
-                case 2:
-                    Console.WriteLine("Snake");
-                    position -= dice;
-                    break;
+                int dice = random.Next(1, 7);
+                Console.WriteLine("\nPlayer rolls dice :" + dice);
+                int option = random.Next(0, 3);
+                switch (option)
+                {
+                    case 0:
+                        Console.WriteLine("No Play");
+                        break;
+                    case 1:
+                        Console.WriteLine("Ladder");
+                        position += dice;                      
+                        break;
+                    case 2:
+                        Console.WriteLine("Snake");
+                        position -= dice;
+                        if (position < 0)
+                        {
+                            position = 0;
+                        }
+                        break;
+                }
+                Console.WriteLine("Player Current Position : " + position);
             }
-            Console.WriteLine("Player Current Position : " + position);
-        }
-    }
+            Console.WriteLine("Player Position : " + position);
+        }        
+    } 
 }
